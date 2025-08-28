f88affaec1f6908e874c50c32c94e6bcd4689da7
Mateo Pretelli <86113904+mateopretelli@users.noreply.github.com>
2025-08-28 17:35:09 -0300

    Register with hashing and JWT + Login with JWT

--- backend/src/config/middlewares.ts
+++ backend/src/config/middlewares.ts
@@ -5,19 +5,24 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 interface RequestWithUser extends Request {
-    user?: any;
+    user?: any; // deberiamos definir el tipado adecuado de user
 }
 
 export async function verifyToken(req: RequestWithUser, res: Response, next: NextFunction) {
-    const token = req.headers.authorization?.split(' ')[1]; //Del header, nos quedamos con el authorization: Bearer, el ? pregunta si existe, de lo contrario undefined y el split separa el bearer del token y nos quedamos con este ultimo
-    if (!token) return res.status(401).send('Unauthorized');
+
+    const token = req.headers.authorization?.split(' ')[1]; 
+    //Del header, nos quedamos con el authorization: Bearer, el ? pregunta si existe, de lo contrario undefined y el split separa el bearer del token y nos quedamos con este ultimo
+    if (!token) return res.status(401).json({ message: 'Unauthorized' });
 
     try {
         const decodedToken = jwt.verify(token, process.env.JWT_SECRET as jwt.Secret);
         req.user = decodedToken;
         next();
     } catch (error) {
+        if (error instanceof jwt.TokenExpiredError) {
+            return res.status(401).json({ message: 'Token expirado' }); // si el token expira, devuelve error
+        }
         console.error('Error verifying token:', error);
-        res.status(401).send('Token inválido');
+        res.status(401).json({ message: 'Token inválido' });
     }
 }
